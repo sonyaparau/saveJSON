af5d034856de6b666823f9eb0fe5d73de54955dd
Sonya Parau <sonyaparau@yahoo.com>
2019-08-06 14:23:35 +0300

    First commit

--- src/main/java/org/msg/group/savejson/Controller.java
+++ src/main/java/org/msg/group/savejson/Controller.java
@@ -1,7 +1,17 @@
 package org.msg.group.savejson;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 //import com.fasterxml.jackson;
 
@@ -15,16 +25,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/saveJSON")
 public class Controller {
 
-//    @RequestMapping(method = RequestMethod.POST)
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @ResponseBody
-//    public ResponseEntity add(@RequestBody Zboruri zbor){
-//        Zboruri zborAdaugat = zborRepository.adauga(zbor);
-//        if(zborAdaugat != null)
-//            return new ResponseEntity<String>("Zborul a fost adaugat cu succes!", HttpStatus.OK);
-//        else
-//            return new ResponseEntity<String>("Eroare la adaugarea zborului!", HttpStatus.NOT_ACCEPTABLE);
-//
-//    }
+    private static final String FILE_NAME = "C:\\poze";
+
+    @RequestMapping(value = "file", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public List<String> save(@RequestBody FileWrapper wrapper) {
+        List<String> response = new ArrayList<String>();
+        for (File file : wrapper.getFiles()) {
+            String pathOfDownload = file.getPath();
+            try (BufferedInputStream in = new BufferedInputStream(new URL(pathOfDownload).openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+                byte dataBuffer[] = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            InputStream in = null;
+            try {
+                in = new URL(pathOfDownload).openStream();
+                Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return response;
+    }
 
 }
---
+++ src/main/java/org/msg/group/savejson/FileWrapper.java
@@ -0,0 +1,24 @@
+package org.msg.group.savejson;
+
+import java.util.List;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; User Name.
+ * @since 19.1.2
+ */
+public class FileWrapper {
+
+    private List<File> files;
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+
+}
